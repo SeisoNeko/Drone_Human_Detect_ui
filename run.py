65f42c5e7d1849a6791129c7f84685f3249480ea
morikon <leafpmpmp@gmail.com>
2024-11-24 16:52:24 +0800

    improve the code compatibility with native python env

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 import os
 
 if __name__ == '__main__':
-    os.system('streamlit run main.py --server.maxUploadSize 10000')
+    try:
+        r = os.system('streamlit run main.py --server.maxUploadSize 10000')
+        if r != 0:
+            os.system('python -m streamlit run main.py --server.maxUploadSize 10000')
+    except:
+        pass
