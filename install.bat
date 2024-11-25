9693dd772f12df736d81b1cb8d94fa0b89419834
morikon <leafpmpmp@gmail.com>
2024-11-25 14:27:50 +0800

    Translated into Chinese(Traditional) with encoding 65001.

--- install.bat
+++ install.bat
@@ -1,12 +1,12 @@
 @echo off
-
+chcp 65001 >nul
 REM 檢查是否已安裝 Python
-WHERE pip
-IF %ERRORLEVEL% NEQ 0 ECHO [101mPython is not installed. Please install Python first.[0m && PAUSE && EXIT /B 1
+WHERE pip >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 ECHO [101mPython未安裝，請先安裝Python...[0m && PAUSE && EXIT /B 1
 
 REM 安裝CUDA Toolkit 11.8
 WHERE nvcc >nul 2>&1
-IF %ERRORLEVEL% EQU 0 (ECHO "CUDA is already installed.")ELSE start /wait cuda_11.8.0_windows_network.exe
+IF %ERRORLEVEL% EQU 0 (ECHO [92mCUDA已安裝...[0m)ELSE start /wait cuda_11.8.0_windows_network.exe
 
 REM 安裝具有 CUDA 支持的 PyTorch、TorchVision 和 Torchaudio
 pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu118
@@ -14,5 +14,5 @@ pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https
 REM 安裝其他依賴
 pip install -r requirements.txt
 
-echo [92mInstallation complete.[0m
+echo [92m安裝完成...[0m
 PAUSE
